f6e80556132339c1250c58fd01fe2d0570253a6d
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-13 22:25:33 +0000

    add css rules and install sass

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { FaFacebookSquare, FaTwitter, FaInstagram, FaYoutube, FaPhoneVolume } from 'react-icons/fa';
+import { FaLocationDot, FaEnvelope } from 'react-icons/fa6';
+
+
+const Header = () => (
+  <header>
+    <ul className="social-media">
+      <li><a href="#" target="_black"><FaFacebookSquare /></a></li>
+      <li><a href="#" target="_black"><FaTwitter /></a></li>
+      <li><a href="#" target="_black"><FaInstagram /></a></li>
+      <li><a href="#" target="_black"><FaYoutube /></a></li>
+    </ul>
+    <ul className="contact-info">
+      <li><FaLocationDot /> Los Angels Gournadi, 1230 Bariasl</li>
+      <li><FaEnvelope /> info@example.com</li>
+      <li><FaPhoneVolume /> +1 (212) 255-5511</li>
+    </ul>
+  </header>
+);
+
+export default Header;
