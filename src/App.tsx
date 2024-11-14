a6d9db3981c54ff7507a5ad0d708d6400809c9d0
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-14 14:58:06 +0000

    add videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header';
+import Home from './components/Home';
 import Menu from './components/Menu';
 
 const App = () => (
   <>
     <Header />
     <Menu />
+    <Home />
   </>
 );
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,30 @@
+import '../styles/home.scss';
+import video1 from '../assets/1.mp4';
+import video2 from '../assets/2.mp4';
+import video3 from '../assets/3.mp4';
+
 const Home = () => {
   return (
-    <section className="home">
-        <div className="home-content">
-          <p>CREATE WHATEVER YOU WANT</p>
-          <h1>Quality leads with the industry!</h1>
-          <div className="btns">
-            <a href="#" className="home-btn">Contact Us</a>
-            <a href="#" className="home-btn">About Us</a>
-          </div>
+    <section className="home-header">
+      <video className="video-slide" src={video1} autoPlay muted loop></video>
+      <div className="slide">
+        <p className="slide-title">CREATE WHATEVER YOU WANT</p>
+        <h1>Quality leads with the industry!</h1>
+        <div className="btns">
+          <a href="#" className="home-btn">Contact Us</a>
+          <a href="#" className="home-btn">About Us</a>
         </div>
+      </div>
+      {/* <div className="slide">
+        <p>CREATE WHATEVER YOU WANT</p>
+        <h1>Quality leads with the industry!</h1>
+        <div className="btns">
+          <a href="#" className="home-btn">Contact Us</a>
+          <a href="#" className="home-btn">About Us</a>
+        </div>
+      </div> */}
+      <button type="button" className="slider-nav next">&#10094;</button >
+      <button type="button" className="slider-nav prev">&#10095;</button >
     </section>
   )
 };
