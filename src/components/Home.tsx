4660efdbe8570306cf0ddb5768b0faf30f397b48
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-14 08:48:58 +0000

    Add home images

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="home">
+        <div className="home-content">
+          <p>CREATE WHATEVER YOU WANT</p>
+          <h1>Quality leads with the industry!</h1>
+          <div className="btns">
+            <a href="#" className="home-btn">Contact Us</a>
+            <a href="#" className="home-btn">About Us</a>
+          </div>
+        </div>
+    </section>
+  )
+};
+
+export default Home;
