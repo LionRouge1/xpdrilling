80c8064f79d0ecfd0c5486cdf36a502cc3bd03eb
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-14 01:56:10 +0000

    Add humbeger menu and make header responsive

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,10 @@
+import { RxHamburgerMenu } from 'react-icons/rx';
 import '../styles/menu.scss';
 
 const Menu = () => (
   <nav className="navbar">
     <img src="https://colza-demo.pbminfotech.com/demo3/wp-content/uploads/sites/9/2021/10/colza-logo.svg" alt="Colza" />
-    <ul>
+    <ul className="nav-links">
       <li>
         <a href="/">Home ˅</a>
       </li>
@@ -17,6 +18,10 @@ const Menu = () => (
         <a href="/contact">Contact Us</a>
       </li>
     </ul>
+
+    <div className="mobile-menu">
+      <RxHamburgerMenu />
+    </div>
 
     <a href="#" className="inqurey-btn">
       <span className="inquery-txt">INQUERY</span>
