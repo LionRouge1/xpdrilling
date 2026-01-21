bbd27391626876cd822dfa63937f71f95617b317
Matchoudi Avlessi <matchoudiavlessi@gmail.com>
2026-01-21 18:37:26 +0100

    Add vite to prerender each page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { htmlPrerender } from "vite-plugin-html-prerender"
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlPrerender({
+      staticDir: path.resolve(__dirname, 'dist'),
+      routes: [
+        '/',
+        '/about-us',
+        '/services',
+        '/fleet',
+        '/our-team',
+        '/contact-us',
+        '/hseq',
+        '/sustainability',
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    target: 'es2022',
+  },
 })
